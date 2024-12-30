37305de7dba247db68c3d943958266146d8b7f81
yugaro22 <yugarotto2525@muyudo.com>
2024-12-30 18:10:14 +0900

    jsの処理修正

--- js/main.js
+++ js/main.js
@@ -2,10 +2,27 @@ const objects = document.querySelectorAll(".animation");
 
 const scrollEvent = function (entries) {
   entries.forEach((entry) => {
+    let action = entry.target.dataset.action;
+    const lottieElement = entry.target; // dotlottie-wc 要素
+    const dotLottie = lottieElement.dotLottie; // dotLottie インスタンスを取得
+
+    if (!dotLottie) {
+      console.error("dotLottieインスタンスが見つかりません。");
+      return;
+    }
+
     if (entry.isIntersecting) {
-      entry.target.classList.add("active");
+      if (action === "lottie") {
+        dotLottie.play(); // dotLottieインスタンスを使って再生
+      } else {
+        lottieElement.classList.add("active"); // スクロール感知で「active」のクラス名を付与
+      }
     } else {
-      entry.target.classList.remove("active");
+      if (action === "lottie") {
+        dotLottie.pause(); // dotLottieインスタンスを使って停止
+      } else {
+        lottieElement.classList.remove("active"); // スクロール感知で「active」のクラス名を削除
+      }
     }
   });
 };
